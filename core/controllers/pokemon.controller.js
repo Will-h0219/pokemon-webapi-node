af60e957348ac387bb4f8ca8d0c4c1a72a24f0b6
Will-h0219 <wherrera0219@gmail.com>
2023-11-02 16:53:43 -0500

    create pokemon endpoint, pagination for getPokemons

--- core/controllers/pokemon.controller.js
+++ core/controllers/pokemon.controller.js
@@ -1,10 +1,43 @@
 const Pokemon = require('../models/pokemon.model');
 
 const getPokemons = async (req, res) => {
-  const results = await Pokemon.find();
-  res.json({ results });
+  let page = req.query.page || 1;
+  page = isNaN(page) || parseInt(page) < 0 ? 1 : page;
+  try {
+    const count = await Pokemon.countDocuments();
+    const results = await Pokemon.find()
+      .limit(5)
+      .skip((page - 1) * 5);
+    const hasPrevious = parseInt(page) !== 1 && parseInt(page) >= 0;
+    const hasNext = (parseInt(page) * 5) <= count;
+    const nextPageUrl = `${req.protocol}://${req.get('host')}${req.baseUrl}?page=${parseInt(page) + 1}`;
+    res.json({
+      count,
+      hasPrevious,
+      hasNext,
+      results,
+      nextPageUrl: hasNext ? nextPageUrl : null
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "We couldn't process your request." });
+  }
+}
+
+const createPokemon = async (req, res) => {
+  const pokemon = new Pokemon(req.body);
+  try {
+    const pokemonDb = await pokemon.save();
+    res.json({
+      result: pokemonDb.id
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "We couldn't process your request." })
+  }
 }
 
 module.exports = {
-  getPokemons
+  getPokemons,
+  createPokemon
 }
--- core/routes/pokemon.routes.js
+++ core/routes/pokemon.routes.js
@@ -4,10 +4,21 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validateFields } = require('../../middlewares/validate-fields.middleware');
-const { getPokemons } = require('../controllers/pokemon.controller');
+const { getPokemons, createPokemon } = require('../controllers/pokemon.controller');
 
 const router = Router();
 
 router.get('/', getPokemons);
+router.post('/',
+  [
+    check('name', 'El nombre es obligatorio').notEmpty(),
+    check('attack', 'El valor no es valido').notEmpty().isInt({ min: 0 }),
+    check('defense', 'El valor no es valido').notEmpty().isInt({ min: 0 }),
+    check('hp', 'El valor no es valido').notEmpty().isInt({ min: 0 }),
+    check('type', 'El tipo es obligatorio').notEmpty(),
+    validateFields
+  ],
+  createPokemon
+);
 
 module.exports = router;
